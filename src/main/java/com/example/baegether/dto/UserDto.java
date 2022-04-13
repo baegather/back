03831e990e6ab5919fa9fd3d75ca73ffa475f62e
minyoung62 <tnd05106@naver.com>
2022-04-13 23:18:02 +0900

    no message

--- src/main/java/com/example/baegether/dto/UserDto.java
+++ src/main/java/com/example/baegether/dto/UserDto.java
@@ -1,13 +1,15 @@
 package com.example.baegether.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
+@Setter
+@Data
+@NoArgsConstructor
 public class UserDto {
-
+    @JsonProperty("user_id")
     private Long id;
 
 }
